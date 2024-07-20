ed5820fbe2c02c8b46bafb5a20ec54e0e89a8d27
aksinghania <akshtsinghnia@gmail.com>
2024-07-20 15:15:21 +0530

    added words and hints

---
+++ scripts/word-list.js
@@ -0,0 +1,74 @@
+const wordList = [
+  {
+    word: "guitar",
+    hint: "A musical instruments with strings",
+  },
+  {
+    word: "oxygen",
+    hint: "A colorless, odorless gas essential for life",
+  },
+  {
+    word: "mountain",
+    hint: "A large natural elevation of the Earth's surface",
+  },
+  {
+    word: "painting",
+    hint: "An art form using colors on a surface to create images or expressions",
+  },
+  {
+    word: "astronomy",
+    hint: "The scientific study of celestial objects and phenomena",
+  },
+  {
+    word: "football",
+    hint: "A popular sport played with a spherical ball",
+  },
+  {
+    word: "chocolate",
+    hint: "A sweet treat made from cocoa beans ",
+  },
+  {
+    word: "butterfly",
+    hint: "A insect with colourful wings and a slender body",
+  },
+  {
+    word: "history",
+    hint: "The study of past events and human civilization",
+  },
+  {
+    word: "pizza",
+    hint: "A savory dish consisting of a round, flattened base with toppings",
+  },
+  {
+    word: "camera",
+    hint: "A device use to capture and record images and videos",
+  },
+  {
+    word: "diamond",
+    hint: "A precious gemstone known for its brilliance and hardness",
+  },
+  {
+    word: "adventure",
+    hint: "An exciting or daring experience",
+  },
+  {
+    word: "science",
+    hint: "The systematic study of the structure and behaviour of the physical and  ",
+  },
+  {
+    word: "bicycle",
+    hint: "A human powered vehicle with two wheels",
+  },
+  {
+    word: "sunset",
+    hint: "The daily dissapearance of the sun below the horizon",
+  },
+  {
+    word: "coffee",
+    hint: "A popular caffeinated beverage made from roasted coffee beans",
+  },
+  {
+    word: "dance",
+    hint: "A rhythemic movement of the body often performed to music",
+  },
+];
